e64ba51d9b0e5d549c0e565721ca271be0d2a4d5
Victor Luis <victorluissantos@live.com>
2020-01-27 21:54:11 -0300

    last version after insert in sistema lever

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-function add(val) {
-	
+function add_tag(val) {
+	if (val != '') {
+		let textarea = $('#text_message').val()
+		let input = $('txt_msg').val()
+
+		textarea += '{'+val.toLowerCase()+'}'
+		input += '{'+val.toLowerCase()+'}'
+		$('#text_message').val(textarea)
+		$('txt_msg').val(input)
+		$('#textarea').focus()
+	}
 }
